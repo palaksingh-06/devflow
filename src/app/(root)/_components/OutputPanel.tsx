f86ab81c93b428b5cbd69e9d668a89ebcf911c53
Palak Singh <palak.singh6122004@gmail.com>
2026-09-08 10:32:56 +0530

    added the handlecopy functionality in output panel

--- src/app/(root)/_components/OutputPanel.tsx
+++ src/app/(root)/_components/OutputPanel.tsx
@@ -1,6 +1,20 @@
-import React from 'react'
-
+"use client";
+import { useCodeEditorStore } from "@/store/useCodeEditorStore";
+import { tr } from "framer-motion/client";
+import { useState } from "react";
 const OutputPanel = () => {
+  const{output,error,isRunning}=useCodeEditorStore();
+  const [isCopied, setIsCopied]=useState(false);
+  const hasContent =error || output;
+
+  const handlecopy=async ()=>{
+    if(!hasContent)
+      return;
+    await navigator.clipboard.writeText(error || output);
+    setIsCopied(true);
+
+    setTimeout(() =>  setIsCopied(false),2000);
+  }
   return (
     <div>OutputPanel</div>
   )
